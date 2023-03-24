aed3fe83627285f6107e7aaae89db21eddcacd0e
Aliaksandr Kamarou <alexander.komarov@innowise-group.com>
2023-03-24 17:27:38 +0100

    start to repository pattern

---
+++ db_repository.py
@@ -0,0 +1,41 @@
+import mysql.connector as connection
+
+
+class RoomRepository:
+    def __init__(self, user, password, host, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
+
+    def connect(self):
+        # Make a connection
+        self.db_connect = connection.connect(
+            user = self.user,
+            password = self.password,
+            host = self.host,
+            database = self.database
+        )
+
+    def disconnect(self):
+        # Make a disconnection
+        self.db_connect.close()
+
+    def get_rooms_with_num_students(self):
+        self.connect()
+        cursor = self.db_connect.cursor()
+
+        sql_query = 'SELECT rooms.name, COUNT(students.id) as number_of_students' \
+                    'FROM rooms' \
+                    'INNER JOIN students ON rooms.id = students.room' \
+                    'GROUP BY rooms.id'
+        cursor.execute(sql_query)
+
+        # Put the result into dict
+        result = {}
+        for row in cursor.fetchall(): # fetchall() return tuple(room, number_of_students)
+            result[row[0]] = row[1]
+
+        self.disconnect()
+        return result
+
