fd1e1dbcf17c395e5bb0a1aaae48d48d71a1b7e5
Aliaksandr Kamarou <alexander.komarov@innowise-group.com>
2023-03-28 14:41:25 +0200

    add unit tests

---
+++ tests/test_db_repository.py
@@ -0,0 +1,31 @@
+import json
+import os
+from db_repository import *
+import mysql.connector as connection
+
+
+password = os.environ.get('PASSWORD_FOR_PYTHON_INTRO')
+user = os.environ.get('USER_FOR_PYTHON_INTRO')
+host = os.environ.get('HOST_FOR_PYTHON_INTRO')
+database_name = os.environ.get('DATABASE_FOR_PYTHON_INTRO')
+
+
+def test_get_rooms_with_num_students():
+    repo = RoomRepository(user=user,
+                          password=password,
+                          host=host,
+                          database=database_name)
+    result = json.loads(repo.get_rooms_with_num_students())
+    connect_to_db = connection.connect(user=user,
+                                       password=password,
+                                       host=host,
+                                       database=database_name)
+    cursor = connect_to_db.cursor()
+    cursor.execute('SELECT COUNT(*) FROM rooms')
+    number_of_rooms = cursor.fetchone()[0]
+    cursor.close()
+    connect_to_db.close()
+
+    assert len(result) == number_of_rooms, \
+        f"The number of real records {number_of_rooms}, records returned by the function is {len(result)}"
+
